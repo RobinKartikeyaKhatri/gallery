491ae24fc2926a6af9c555e45f96dc810d7e5d78
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-17 14:04:00 +0530

    removed if() conditional part code

--- admin/includes/new_config.php
+++ admin/includes/new_config.php
@@ -6,12 +6,7 @@ define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "gallery_db");
 
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-if(!$connection)
-{
-    die("Connection failed " . mysqli_error($connection));
-}
 
 
 ?>
