b646b022095a8b1d3eb27611c14ed92289dff8f9
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-18 20:38:43 +0530

    created definition for has_the_attribute() method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -50,6 +50,13 @@ class User
 
         return $the_object;
     }
+
+    private function has_the_attribute($the_attribute)
+    {
+        $object_properties = get_object_vars($this);
+
+        return array_key_exists($the_attribute, $object_properties);
+    }
 }
 
 
