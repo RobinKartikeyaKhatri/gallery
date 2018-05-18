545144b934358cb9745228aecf45103b77498e49
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-18 08:23:35 +0530

    created user.php class and a method find_all_users()

---
+++ admin/includes/user.php
@@ -0,0 +1,25 @@
+<?php
+
+class User
+{
+    public function find_all_users()
+    {
+        global $database;
+
+        $result_set = $database->query("SELECT * FROM users");
+
+        return $result_set;
+
+        // while($row = mysqli_fetch_array($result))
+        // {
+        //     $id         = $row['id'];
+        //     $username   = $row['username'];
+        //     $password   = $row['password'];
+        //     $firstname  = $row['firstname'];
+        //     $lastname   = $row['lastname'];
+        // }
+    }
+}
+
+
+?>
