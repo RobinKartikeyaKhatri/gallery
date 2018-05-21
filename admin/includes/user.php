1667c83ed1fc63ca026900809debb82afee33c0b
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-21 09:27:17 +0530

    checked update query for a bug

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -108,12 +108,12 @@ class User
     {
         global $database;
 
-        $sql = "UPDATE USERS SET ";
+        $sql = "UPDATE users SET ";
         $sql .= "username = '" . $database->escape_string($this->username) . "', ";
         $sql .= "password = '" . $database->escape_string($this->password) . "', ";
         $sql .= "first_name = '" . $database->escape_string($this->first_name) . "', ";
         $sql .= "last_name = '" . $database->escape_string($this->last_name) . "' ";
-        $sql .= "WHERE id = " . $database->escape_string($this->id);
+        $sql .= " WHERE id = " . $database->escape_string($this->id);
 
         $database->query($sql);
 
