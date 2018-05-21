578083036c553c8c1a2a16a31004c0ec6ef8f60a
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-21 15:41:02 +0530

    implemented properties() method insde create() method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -99,11 +99,8 @@ class User
         $properties = $this->properties();
         
         $sql = "INSERT INTO " . self::$db_table . "(" . implode(",", array_keys($properties)) .")";
-        $sql .= "VALUES ('";
-        $sql .= $database->escape_string($this->username) . "', '";
-        $sql .= $database->escape_string($this->password) . "', '";
-        $sql .= $database->escape_string($this->first_name) . "', '";
-        $sql .= $database->escape_string($this->last_name) . "')";
+        $sql .= "VALUES ('". implode("','", array_values($properties)) ."')";
+
 
         if($result = $database->query($sql))
         {
