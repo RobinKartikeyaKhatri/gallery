85c1d62cd8f87273d57fc7d22fd8dad0e5116f4e
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-23 16:01:52 +0530

    created save() method

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -4,7 +4,7 @@ class Photo extends Db_object
 {
     protected static $db_table = "photos";
     protected static $db_table_fields = array('photo_id', 'title', 'description', 'filename', 'type', 'size');
-    public $Photo_id;
+    public $photo_id;
     public $title;
     public $description;
     public $filename;
@@ -47,6 +47,31 @@ class Photo extends Db_object
         }
 
         
+    }
+
+    public function save()
+    {
+        if($this->photo_id)
+        {
+            $this->update();
+        }
+        else
+        {
+            if(!empty($this->errors))
+            {
+                return false;
+            }
+
+            if(empty($this->filename) || empty($this->tmp_path))
+            {
+                $this->errors[] = "The file was not available";
+                return false;
+            }
+
+            $target_path = SITE_ROOT . DS . 'admin' . DS . $this->upload_directory . DS . $this->filename;
+
+            $this->create();
+        }
     }
 
 }
