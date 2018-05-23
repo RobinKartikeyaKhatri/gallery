3d6151b0c94ceaf6744a75fdb44be0736bc3ce98
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-23 10:34:01 +0530

    replaced self:: to static:: also used get_called_class() function inside instantiation() method

--- admin/includes/db_object.php
+++ admin/includes/db_object.php
@@ -2,14 +2,17 @@
 
 class Db_object
 {
+
+    protected static $db_table = "users";
+
     public static function find_all()
     {
-        return self::find_this_query("SELECT * FROM " . self::$db_table . " ");
+        return static::find_this_query("SELECT * FROM " . static::$db_table . " ");
     }
 
     public static function find_by_id($user_id)
     {
-        $the_result_array = self::find_this_query("SELECT * FROM " . self::$db_table . " WHERE id = {$user_id} LIMIT 1");
+        $the_result_array = static::find_this_query("SELECT * FROM " . static::$db_table . " WHERE id = {$user_id} LIMIT 1");
         return !empty($the_result_array) ? array_shift($the_result_array) : false;
     }
 
@@ -23,7 +26,7 @@ class Db_object
 
         while($row = mysqli_fetch_array($result_set))
         {
-            $the_object_array[] = self::instantiation($row);
+            $the_object_array[] = static::instantiation($row);
         }
 
         return $the_object_array;
@@ -33,7 +36,8 @@ class Db_object
 
     public static function instantiation($the_record)
     {
-        $the_object = new self;
+        $calling_class = get_called_class();
+        $the_object = new $calling_class;
 
         foreach ($the_record as $the_attribute => $value) 
         {
