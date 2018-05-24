57979f41208bd9193d0e7155642f0d6ffa2f7ba0
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-24 09:21:38 +0530

    changed photo_id to id

--- admin/photos.php
+++ admin/photos.php
@@ -54,12 +54,12 @@ $photos = Photo::find_all();
                                                    <tr>
                                                    <td><img class="img-responsive" width="200" src="<?php echo $photo->picture_path(); ?>" alt="">
                                                         <div class="pictures_link">
-                                                            <a href="delete_photo.php?id=<?php echo $photo->photo_id; ?>" class="btn btn-danger">Delete</a>
+                                                            <a href="delete_photo.php?id=<?php echo $photo->id; ?>" class="btn btn-danger">Delete</a>
                                                             <a href="#" class="btn btn-warning">Edit</a>
                                                             <a href="#" class="btn btn-success">View</a>
                                                         </div>
                                                    </td>
-                                                   <td><?php echo $photo->photo_id; ?></td>
+                                                   <td><?php echo $photo->id; ?></td>
                                                    <td><?php echo $photo->filename; ?></td>
                                                    <td><?php echo $photo->title; ?></td>
                                                    <td><?php echo $photo->size; ?></td>
