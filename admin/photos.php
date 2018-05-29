24f7998b9c76bf9161cc081d0fd7d48d864a37d8
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-29 23:05:47 +0530

    created a link to go to specific comment

--- admin/photos.php
+++ admin/photos.php
@@ -65,10 +65,12 @@ $photos = Photo::find_all();
                                                    <td><?php echo $photo->title; ?></td>
                                                    <td><?php echo $photo->size; ?></td>
                                                    <td>
+                                                        <a class="btn btn-success" href="comment_photo.php?id=<?php echo $photo->id; ?>">
                                                         <?php 
                                                             $comments = Comment::find_the_comments($photo->id);
                                                             echo count($comments);
                                                         ?>
+                                                        </a>
                                                    </td>
                                                    </tr>
 
