41d0a342f5f80530fa353fec051a7904c4c1fa7f
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-30 13:46:33 +0530

    initailized properties

--- admin/includes/paginate.php
+++ admin/includes/paginate.php
@@ -2,15 +2,15 @@
 
 class Paginate
 {
-    public $page;
+    public $current_page;
     public $items_per_page;
     public $items_total_count;
 
-    public function __construct($page=1, $items_per_page=10, $items_total_count=0)
+    public function __construct($page=1, $items_per_page=4, $items_total_count=0)
     {
-        $this->page = $page;
-        $this->items_per_page = $items_per_page;
-        $this->items_total_count = $items_total_count;
+        $this->current_page = (int)$page;
+        $this->items_per_page = (int)$items_per_page;
+        $this->items_total_count = (int)$items_total_count;
     }
 }
 
