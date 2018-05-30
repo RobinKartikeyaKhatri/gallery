db0262a6346971e5e2e7658821bab9d47ef98c08
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-30 10:27:06 +0530

    called find_all() method, created a foreach loop to access data, created HTML skeleton for image display

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php include("includes/header.php"); ?>
 
+<?php 
+$photos = Photo::find_all();
+?>
+
 
         <div class="row">
 
             <!-- Blog Entries Column -->
-            <div class="col-md-8">
-
-    
-            
-          
-         
-
+            <div class="col-md-12">
+                <?php foreach ($photos as $photo): ?>
+                  <div class="thumbnail row">
+                      <div class="col-xs-6 col-md-3">
+                        <a class="thumbnail" href="#">
+                          <img src="" alt="">
+                        </a>
+                      </div>
+                  </div>
+                <?php endforeach?>
             </div>
 
 
