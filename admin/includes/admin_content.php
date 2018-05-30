2f6c82d3a5c3ed66b665d3bc7372624fdd982594
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-30 09:56:34 +0530

    Changed heading to 'Admin Dashboard'

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -4,8 +4,8 @@
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">
-                Blank Page
-                <small>Subheading</small>
+                Admin
+                <small>Dashboard</small>
             </h1>
 
             <div class="row">
