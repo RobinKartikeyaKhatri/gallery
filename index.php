45eb171fe5c4aa4aaee579b20f2025b908920a6f
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-30 14:54:23 +0530

    created ul element and inside it used page_total() and has_next() method for display next link

--- index.php
+++ index.php
@@ -40,6 +40,27 @@ $photos = Photo::find_by_query($sql);
                 
                 <?php endforeach?>
                 </div>
+
+                <div class="row">
+                  <ul class="pager">
+                    
+                    <?php
+                    
+                      if($paginate->page_total() > 1)
+                      {
+                          if($paginate->has_next())
+                          {
+                            echo "<li class='next'><a href=''>Next</a></li>";
+                          }
+                      }
+                    
+                    ?>
+
+                    
+                    <li class='previous'><a href=''>Previous</a></li>
+                  </ul> 
+                </div>
+
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
