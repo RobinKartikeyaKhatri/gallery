cf5c51450aa8bcba1f5a23f35df487fd385ccce2
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-06-02 17:04:21 +0530

    linked dropzone.css file

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -32,6 +32,8 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+    <link rel="stylesheet" href="css/dropzone.css">
+
     <link rel="stylesheet" href="css/styles.css">
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
