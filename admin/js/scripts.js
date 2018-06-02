3a2c0eb9f5a5ad7e3805b80f9773dce02a6420c7
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-06-02 06:13:15 +0530

    created AJAX call for photo id

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -19,13 +19,13 @@ $(document).ready(function(){
         image_href_splitted = image_src.split("/");
         image_name = image_href_splitted[image_href_splitted.length -1];
 
-        photo_id = $(this).attr('data');
+        photo_id = $(this).attr("data");
 
         $.ajax({
             url: "includes/ajax_code.php",
-            data: {photo_id: photo_id},
+            data:{photo_id:photo_id},
             type: "POST",
-            success:function(data){
+            success: function(data){
                 if(!data.error){
                     $("#modal_sidebar").html(data);
                 }
