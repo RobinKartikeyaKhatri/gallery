a077442f25c3760364087abdb229a863ad4f2448
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-06-02 16:19:19 +0530

    called message() method

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -21,6 +21,7 @@ $photo = Photo::find_by_id($_GET['id']);
 if($photo)
 {
     $photo->delete_photo();
+    $session->message("The {$photo->filename} has been deleted.");
     redirect("photos.php");
 }
 else
